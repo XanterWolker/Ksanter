c2272750816889cd49be01b85c4693bb33f1885d
XanterWolker <62740435+XanterWolker@users.noreply.github.com>
2020-04-06 14:44:26 +0700

    Add files via upload

--- Вот ЗМЕЙ/Вот ЗМЕЙ/Program.cs
+++ Вот ЗМЕЙ/Вот ЗМЕЙ/Program.cs
@@ -1,80 +1,141 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Threading;
 namespace Змеюка
 {
     class Program
     {
-        int Height = 20;
-        int Width = 30;
+       static public int height = 20;
+       static public int width = 30;
+        public ConsoleKeyInfo keypress = new ConsoleKeyInfo();
+        static public bool gameOver, horizontal, vertical, isprinted;
 
-        int[] X = new int[50];
-        int[] Y = new int[50];
+      static public int[] TailX = new int[50];
+      static public int[] TailY = new int[50];
+        int speed = 0;
+       
+       
 
-        int foodX;
-        int foodY;
+        static public int foodX, foodY, headX, headY, score,w, h, x, y, nTail;
+         
+        public enum Direction { Stop, Left, Right, Up, Down};
+        static public Direction dir;
+        static Direction Stop = 0;
 
-        int elementsss = 3;
-
-        ConsoleKeyInfo KeyInfo = new ConsoleKeyInfo();
-        char key = 'W';
-
-        Random rnd = new Random();
-
-        Program()
+        static public void ssset()
         {
-            X[0] = 5;
-            Y[0] = 5;
-            Console.CursorVisible = false;
-            foodX = rnd.Next(2, (Width - 2));//размещение еды
-            foodY = rnd.Next(2, (Height - 2));//размещение еды
+            dir = Stop;
+            gameOver = false;
+
+            w = width / 2 - 1;
+            h = height / 2 - 1;
+
+            headX = width / 2;
+            headY = height / 2;
+
+            var rnd = new Random();
+            foodX = rnd.Next() % width;
+            foodY = rnd.Next() % height;
+
+            score = 0;
+            nTail = 0;
         }
+
+
+
 
         public void WriteBoard()
         {
-            Console.Clear();
-            for (int i = 1; i <= (Width + 2); ++i)
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < height; ++i)
             {
-                Console.SetCursorPosition(i, 1);
-                Console.Write("*");
+                for (int j = 0; j < width; ++j)
+                {
+                    if (i == 0 || i == height - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else if (j == 0 || j == width - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else if (j == foodX && i == foodY)
+                    {
+                        Console.Write("P");
+                    }
+                    else if (j == headX && i == headY)
+                    {
+                        Console.Write("$");
+                    }
+                    else
+                    {
+                        isprinted = false;
+                        for (int k = 0; k < nTail; ++k)
+                        {
+                            if (TailX[k] == j && TailY[k] == i)
+                            {
+                                Console.Write("o");
+                                isprinted = true;
+                            }
+                        }
+                        if (!isprinted)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+
+
+
+                }
+                Console.WriteLine();
 
             }
-            for (int i = 1; i <= (Width + 2); ++i)
-            {
-                Console.SetCursorPosition(i, (Height + 2));
-                Console.Write("*");
-            }
-            for (int i = 1; i <= (Height + 1); i++)
-            {
-                Console.SetCursorPosition(1, i);
-                Console.Write("*");
-            }
-            for (int i = 1; i <= (Height + 1); ++i)
-            {
-                Console.SetCursorPosition((Width + 2), i);
-                Console.Write("*");
-            }
-
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("        Shake Game              ");
+            Console.WriteLine();
+            Console.WriteLine("Your score:" + score);
         }
-
         public void Input()
         {
-            if (Console.KeyAvailable)//проверка нажатия клавиши
+
+            ConsoleKey keypress = Console.ReadKey(true).Key;
+            switch (keypress)
             {
-                KeyInfo = Console.ReadKey(true);//нажатие клавиши
-                key = KeyInfo.KeyChar;//замена клавиши на значение Char
+                case ConsoleKey.UpArrow:
+                    dir = Direction.Up;
+                    break;
+                case ConsoleKey.DownArrow:
+                    dir = Direction.Down;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    dir = Direction.Left;
+                    break;
+                case ConsoleKey.RightArrow:
+                    dir = Direction.Right;
+                    break;
+                case ConsoleKey.Escape:
+                    gameOver = true;
+                    break;
             }
         }
 
         public void difficult()
         {
 
-            Console.WriteLine("Выберите уровень сложности:" +
+            Console.WriteLine("                        Приветствую в игре повелитель змея");
+            Console.WriteLine("                            Выбери уровень сложности:" +
 
-            "\n1 - Easy peasy" +
+            "\n                                  1 - Easy peasy" +
 
-            "\n2 - Medium" +
+            "\n                                   2 - Medium" +
 
-            "\n3 - Death");
+            "\n                                    3 - Death");
+
+
+
 
             int D = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
@@ -82,21 +143,25 @@ namespace Змеюка
             switch (D)
             {
                 case 1:
-                    Height = 10;
-                    Width = 10;
+                    speed = 1;
                     Thread.Sleep(100);
+                    height = 10;
+                    width = 10;
+                    
                     break;
 
                 case 2:
-                    Height = 15;
-                    Width = 15;
-                    Thread.Sleep(200);
+                    speed = 2;
+                    height = 15;
+                    width = 15;
+                   
                     break;
 
                 case 3:
-                    Height = 20;
-                    Width = 20;
-                    Thread.Sleep(400);
+                    speed = 3;
+                    height = 20;
+                    width = 20;
+                   
                     break;
 
             }
@@ -112,46 +177,147 @@ namespace Змеюка
             Console.Write("$");
         }
 
+
+
+
+
+
         public void Logic()
         {
-            if (X[0] == foodX)
+            int PreX = TailX[0];
+            int PreY = TailY[0];
+            int tempX, tempY;
+
+            if (dir != Stop)
             {
-                if (Y[0] == foodY)
+                TailX[0] = headX;
+                TailY[0] = headY;              
+            }
+            for (int i = 1; i<nTail; ++i )
+            {
+                tempX = TailX[i];
+                tempY = TailY[i];
+                TailX[i] = PreX;
+                TailY[i] = PreY;
+                PreX = tempX;
+                PreY = tempY;
+            }
+
+
+            if (headX <= 0 || headX >= width - 1 || headY <= 0 || headY >= height - 1)
+            {
+                gameOver = true;
+            }
+            else
+            {
+                gameOver = false;
+            }
+
+            if (headX == foodX && headY == foodY)
+            {
+
+                    score += 10;
+                    nTail++;
+                    var rnd = new Random();
+                    foodX = rnd.Next() % width;//размещение еды
+                    foodY = rnd.Next() % height;//размещение еды
+                
+            }
+            if (((dir == Direction.Up && dir != Direction.Left) && (dir == Direction.Up && dir != Direction.Right)) || ((dir == Direction.Down && dir != Direction.Left) && (dir == Direction.Down && dir != Direction.Right)))
+            {
+                horizontal = true;
+            }
+            else
+            {
+                horizontal = false;
+            }
+
+            if (((dir == Direction.Up && dir != Direction.Left) && (dir == Direction.Up && dir != Direction.Right)) || ((dir == Direction.Down && dir != Direction.Left) && (dir == Direction.Down && dir != Direction.Right)))
+            {
+                vertical = true;
+            }
+            else
+            {
+                vertical = false;
+            }
+
+            for (int i = 1; i < nTail; ++i)
+            {
+                if (TailX[i] == headX && TailY[i] == headY)
                 {
-                    elementsss++;
-                    foodX = rnd.Next(2, (Width - 2));//размещение еды
-                    foodY = rnd.Next(2, (Height - 2));//размещение еды
+                    if (horizontal || vertical)
+                    {
+                        gameOver = false;
+                    }
+                    else
+                    {
+                        gameOver = true;
+                    }
                 }
-            }
+                var rnd = new Random();
+                if (TailX[i] == foodX && TailY[i] == foodY)
+                {
+                    foodX = rnd.Next(1, width - 1);
+                    foodY = rnd.Next(1, height - 1);
+                }
 
-            for (int i = elementsss; i > 1; i--)//установка последовательности элементов змеи
-            {
-                X[i - 1] = X[i - 2];
-                Y[i - 1] = Y[i - 2];
-            }
-            switch (key)
-            {
-                case 'w':
-                    Y[0]--;
-                    break;
-                case 's':
-                    Y[0]++;
-                    break;
-                case 'd':
-                    X[0]++;
-                    break;
-                case 'a':
-                    X[0]--;
-                    break;
+
 
             }
-            for (int i = 0; i <= (elementsss - 1); i++)//появление змеюки
+
+
+
+            for (int i = nTail; i > 1; i--)//установка последовательности элементов змеи
             {
-                WritePoint(X[i], Y[i]);
+                TailX[i - 1] = TailX[i - 2];
+                TailY[i - 1] = TailY[i - 2];
+            }
+
+            for (int i = 0; i <= (nTail - 1); i++)//появление змеюки
+            {
+                WritePoint(TailX[i], TailY[i]);
                 WritePoint(foodX, foodY);
 
             }
-            Thread.Sleep(100);
+            
+
+            switch (dir)
+            {
+                case Direction.Stop:
+                    dir = 0;
+                    break;
+                case Direction.Up:
+                    headY-=speed;
+                    break;
+                case Direction.Down:
+                    headY+=speed;
+                    break;
+                case Direction.Right:
+                    headX+=speed;
+                    break;
+                case Direction.Left:
+                    headX-=speed;
+                    break;
+            }
+           
+            
+          
+
+
+
+
+
+        }
+        void Update()
+        {
+            while(!gameOver)
+            {
+                WriteBoard();
+                Logic();
+                Input();
+                
+                
+            }
         }
 
         static void Main(string[] args)
@@ -163,17 +329,20 @@ namespace Змеюка
 
             Program program = new Program();
             program.difficult();
-            Console.Clear();
+            
             while (true)
             {
+               
+                ssset();
+                program.Update();
+                Console.Clear();
+               
+                
 
-                program.WriteBoard();
 
-                program.Input();
-                program.Logic();
 
             }
-            Console.ReadKey();
+            
         }
     }
 }
